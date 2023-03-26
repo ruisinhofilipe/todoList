c403638899aa65ca5ead5f4951cde9126c76015d
Rui Silva <ruisinhofilipe@gmail.com>
2023-03-26 10:52:53 +0200

    Add and display project name sketch

--- src/index.js
+++ src/index.js
@@ -10,13 +10,6 @@ const button = document.createElement('button');
 button.textContent = 'Add project';
 document.body.appendChild(button);
 
-button.addEventListener('click', () => {
-    printMe(projectNames, document.querySelector('.projectNameClass').value);
-    if (document.querySelector('.projectNameClass').value !== '') {
-        document.querySelector('.projectNameClass').value = '';
-    }
-})
-
 const button2 = document.createElement('button');
 button2.textContent = 'show project array';
 document.body.appendChild(button2);
@@ -24,3 +17,30 @@ document.body.appendChild(button2);
 button2.addEventListener('click', () => {
     console.log(projectNames);
 });
+
+const projectDisplay = document.createElement('div');
+projectDisplay.setAttribute('class', 'projectsDisplay');
+projectDisplay.textContent = 'Project';
+document.body.appendChild(projectDisplay);
+
+
+button.addEventListener('click', () => {
+    printMe(projectNames, document.querySelector('.projectNameClass').value);
+    if (document.querySelector('.projectNameClass').value !== '') {
+        let smallDisplay = document.createElement('div');
+        smallDisplay.setAttribute('class', 'smallDisplay');
+        smallDisplay.textContent = projectNames[projectNames.length - 1];
+        projectDisplay.appendChild(smallDisplay);
+
+        document.querySelector('.projectNameClass').value = '';
+    }
+
+})
+
+
+
+
+
+
+
+
