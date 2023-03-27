c214e4f8c3cc64c4137f2d5f7a617be079129acd
Rui Silva <ruisinhofilipe@gmail.com>
2023-03-27 23:10:13 +0200

    Projects array and todo array working separately

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import printMe from "./print";
 import "./styles.css";
 
 
-let projectNames = [];
+let todoArray = [];
+let projectArray = [];
 const mainDiv = document.createElement('div');
 mainDiv.setAttribute('class', 'mainDiv');
 document.body.appendChild(mainDiv);
@@ -13,16 +14,40 @@ projectInput.setAttribute('class', 'projectNameClass');
 mainDiv.appendChild(projectInput);
 
 const button = document.createElement('button');
-button.textContent = 'Add project';
+button.textContent = 'Add todo';
 mainDiv.appendChild(button);
 
 
 const button2 = document.createElement('button');
-button2.textContent = 'show project array';
+button2.textContent = 'show todos array';
 mainDiv.appendChild(button2);
 
+const button3 = document.createElement('button');
+button3.textContent = 'Add projects';
+mainDiv.appendChild(button3);
+
+const button4 = document.createElement('button');
+button4.textContent = 'show projects array';
+mainDiv.appendChild(button4);
+
 button2.addEventListener('click', () => {
-    console.log(projectNames);
+    console.log(todoArray);
+});
+
+button3.addEventListener('click', () => {
+    printMe(projectArray, document.querySelector('.projectNameClass').value);
+    if (document.querySelector('.projectNameClass').value !== '') {
+        let projectsDisplay = document.createElement('div');
+        projectsDisplay.setAttribute('class', 'projectsDisplay');
+        projectsDisplay.textContent = projectArray[projectArray.length - 1];
+        mainDiv.appendChild(projectsDisplay);
+
+        document.querySelector('.projectNameClass').value = '';
+    }
+});
+
+button4.addEventListener('click', () => {
+    console.log(projectArray);
 });
 
 const projectDisplay = document.createElement('div');
@@ -33,11 +58,11 @@ mainDiv.appendChild(projectDisplay);
 
 button.addEventListener('click', () => {
 
-    printMe(projectNames, document.querySelector('.projectNameClass').value);
+    printMe(todoArray, document.querySelector('.projectNameClass').value);
     if (document.querySelector('.projectNameClass').value !== '') {
         let smallDisplay = document.createElement('div');
         smallDisplay.setAttribute('class', 'smallDisplay');
-        smallDisplay.textContent = projectNames[projectNames.length - 1];
+        smallDisplay.textContent = todoArray[todoArray.length - 1];
         projectDisplay.appendChild(smallDisplay);
 
         document.querySelector('.projectNameClass').value = '';
