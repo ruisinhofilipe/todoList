4ca9833d6b7ed233acc9dba2efd9cd2e54104ba9
Rui Silva <ruisinhofilipe@gmail.com>
2023-04-01 15:55:23 +0200

    Project are now displayed without being duplicate

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,17 @@
 import elementFactory from './elementFactory';
 
-function printMe(obj, value) {
+// const TakeCareProjectObject = () => {
+//     this.addProperties = function objectProperties(obj, value) {
+//         if (value !== '') {
+//             obj[value] = [];
+//         }
+
+//         return obj;
+//     };
+
+// }
+
+function objectProperties(obj, value) {
     if (value !== '') {
         obj[value] = [];
     }
@@ -8,24 +19,52 @@ function printMe(obj, value) {
     return obj;
 }
 
+function clearDuplicateProjects() {
+    let allProjects = document.querySelectorAll('.projectsName');
+    for (let i = 0; i < allProjects.length; i++) {
+        allProjects[i].remove();
+    }
+
+}
+
 export function addProjects() {
 
     let projectObj = {};
     const mainDiv = document.querySelector('.mainDiv');
 
+
+    //Project div 
+    const displayProjects = elementFactory('div', { class: 'displayProjects' });
+    mainDiv.appendChild(displayProjects);
+
     // Add project button
     const addProject = elementFactory('button', { class: 'addProjectButton' }, '+');
-    mainDiv.appendChild(addProject);
+    displayProjects.appendChild(addProject);
 
     // Text input to add projects
     const addProjectInput = elementFactory('input', { type: 'text', class: 'addProjectInput' });
-    mainDiv.appendChild(addProjectInput);
+    displayProjects.appendChild(addProjectInput);
 
     // Add project button event listener
     addProject.addEventListener('click', () => {
-        printMe(projectObj, document.getElementsByClassName('addProjectInput')[0].value)
+        let projectIndex = 0;
+
+        // Clear every project DOM element previously created so it doesn't have duplicates on the page
+        clearDuplicateProjects();
+
+        objectProperties(projectObj, document.getElementsByClassName('addProjectInput')[0].value)
         document.getElementsByClassName('addProjectInput')[0].value = '';
-        console.log(projectObj)
+
+        // create a div and display each project
+        for (let keys in projectObj) {
+            const projectName = elementFactory('div', { class: 'projectsName', id: projectIndex }, keys);
+            displayProjects.appendChild(projectName);
+            projectIndex++;
+        }
+
+
+
+
     });
 
     return addProject, addProjectInput;
