a8e9c8388c28af49c725ea1538a4fa934a6d22a8
Rui Silva <ruisinhofilipe@gmail.com>
2023-04-18 19:25:46 +0200

    Changes on the UI, mostly about how projects and todos are added

--- src/projects.js
+++ src/projects.js
@@ -2,19 +2,20 @@ import elementFactory from './elementFactory';
 import clearDuplicate from './clearDuplicate';
 import { todoArray } from './todoFactory';
 import { displayTodos } from './todoFactory';
-import removeDom from './clearPage';
-
 
 export let projectArray = [];
 export let currentProjectIndex;
 
-const projectsDiv = document.querySelector('.projectsDiv');
-
+const projectClass = document.querySelector('.addProject-popup');
+const overlay = document.querySelector('.overlay');
 // Push project to array
 function pushProject(projectName) {
     if (projectName !== null && !projectArray.includes(projectName) && projectName !== '' && projectName !== ' ') {
         projectArray.push(projectName);
         todoArray.push([]);
+        document.getElementById('titleProject').value = '';
+        projectClass.classList.remove('visible');
+        displayArrayProjects();
     } else {
         alert('Please, introduce a valid project name or you that you haven\'t introduced yet.');
     }
@@ -34,67 +35,71 @@ function styleProject() {
 }
 
 function displayArrayProjects() {
+    // Change project input display property
+    const addProject = document.querySelector('.addPrj');
+    addProject.addEventListener('click', () => {
+        const projectClass = document.querySelector('.addProject-popup');
+        projectClass.classList.add('visible');
+        overlay.classList.add('visible');
+    });
+
     clearDuplicate('.projectsNameDiv');
     projectArray.forEach(project => {
         let index = projectArray.indexOf(project);
-        const displayProjects = document.querySelector('.displayProjects');
         const projectNameDiv = elementFactory('div', { class: 'projectsNameDiv', index });
         const projectName = elementFactory('div', { class: 'projectsName' }, project);
         const removeProject = elementFactory('button', { class: 'removeProject' }, 'x');
         projectNameDiv.appendChild(projectName);
         projectNameDiv.appendChild(removeProject);
-        displayProjects.appendChild(projectNameDiv);
+        document.querySelector('.displayProjects').appendChild(projectNameDiv);
 
 
         projectNameDiv.addEventListener('click', () => {
             currentProjectIndex = projectNameDiv.getAttribute('index');
             displayTodos(currentProjectIndex);
             displayArrayProjects();
-            styleProject()
+            styleProject();
         });
 
-        // Remove projects
-        removeProject.addEventListener('click', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            let index = removeProject.parentNode.getAttribute('index');
-            projectArray.splice(index, 1);
-            todoArray.splice(index, 1);
-            if (projectArray.length >= 1) {
-                currentProjectIndex = 0;
-                displayArrayProjects();
-                displayTodos(currentProjectIndex);
-                styleProject();
-            } else {
-                currentProjectIndex = '';
-                displayArrayProjects();
-                removeDom('.displayTodo');
-            }
-        });
+        // // Remove projects
+        // removeProject.addEventListener('click', (e) => {
+        //     e.preventDefault();
+        //     e.stopPropagation();
+        //     let index = removeProject.parentNode.getAttribute('index');
+        //     projectArray.splice(index, 1);
+        //     todoArray.splice(index, 1);
+        //     if (projectArray.length >= 1) {
+        //         currentProjectIndex = 0;
+        //         displayArrayProjects();
+        //         displayTodos(currentProjectIndex);
+        //         styleProject();
+        //     } else {
+        //         currentProjectIndex = '';
+        //         displayArrayProjects();
+        //         removeDom('.displayTodo');
+        //     }
+        // });
     });
 }
 
-
 export function projectDom() {
-    //Add project div 
-    const addPrj = elementFactory('div', { class: 'addPrj' }, undefined,
-        elementFactory('input', { type: 'text', class: 'addProjectInput', placeholder: 'Your new project' }),
-        elementFactory('button', { class: 'addProjectButton' }, 'Create project')
-    );
-    projectsDiv.appendChild(addPrj);
-
-    //Projects display
-    const displayProjects = elementFactory('div', { class: 'displayProjects' });
-    projectsDiv.appendChild(displayProjects);
-
     displayArrayProjects();
 
-    // Add project button event listener
-    const addProjectButton = document.querySelector('.addProjectButton');
-    addProjectButton.addEventListener('click', () => {
-        // push project into array project
-        pushProject(document.getElementsByClassName('addProjectInput')[0].value);
-        document.getElementsByClassName('addProjectInput')[0].value = '';
-        displayArrayProjects();
+    // Confirm new project name button
+    const confirmButton = document.querySelector('.confirmButton');
+    confirmButton.addEventListener('click', () => {
+        const inputValue = document.getElementById('titleProject').value;
+        pushProject(inputValue);
+        console.log(projectArray);
+        overlay.classList.remove('visible');
+        // styleProject();
+    });
+
+    // Cancel new project name button
+    const cancelButton = document.querySelector('.cancelButton');
+    cancelButton.addEventListener('click', () => {
+        const projectClass = document.querySelector('.addProject-popup');
+        projectClass.classList.remove('visible');
+        overlay.classList.remove('visible');
     });
 }
